52ca61dfd59efb3adb086c1c689c7ce2f6cd3fdd
MaryKate <mary.mcgranahan19@mbpr1315-3280.local>
2019-04-09 14:18:21 -0400

    hi

--- API_App/controllers/users.js
+++ API_App/controllers/users.js
@@ -1,16 +1,17 @@
 /*router.get('/user/new',function(request, response){
   console.log('Request- /new user');
+  Users.createUser(request.body.email);
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('user_details',{user:user});
 });
 
-router.post('/user', function(request,response){
-console.log("Post- new user");
-Users.createUser(request.body.name,request.body.password);
+router.post('/results', function(request,response){
+console.log("Post- user w/API Key");
+Users.crea(request.body.name,request.body.password);
 response.status(200);
 response.setHeader('Content-Type', 'text/html')
-response.render('index');
+response.render('results');
 });
 
 */
---
+++ API_App/models/Users.js
@@ -0,0 +1,32 @@
+exports.createUser = function(user_email){
+  console.log("Users.createUser"+user_email);
+  var user_key=createKey();
+  var user={
+    email:user_email,
+    key:user_key
+  };
+  doc.addRow(1,user, function(err) {
+    if(err) {
+      console.log(err);
+    }
+
+  });
+  return user;
+  //fill
+}
+
+function createKey(){
+  var keyString;
+  var numString;
+  for(var i=0; i<7; i++){
+    var num=Math.floor(Math.random() * 33) + 93;
+    if(i%2==0){
+        numString=String.fromCharCode(num);
+        keyString=keyString+num;
+    }
+    else(){
+      numString=num.toString()
+      keyString+=numString;
+    }
+  return(keyString);
+}

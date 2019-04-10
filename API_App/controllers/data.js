5d1f578ccf39119d28702ed6ec42d9a60630d780
MaryKate <mary.mcgranahan19@mbpr1315-3280.local>
2019-04-10 08:56:34 -0400

    stuff

--- API_App/controllers/data.js
+++ API_App/controllers/data.js
@@ -4,8 +4,16 @@ var router = express.Router();
 
 var Users = require('../models/Users');
 
-router.post('/results', function(request,response){
-console.log("Post- results");
+router.get('/searchset1', function(request,response){//Attendence set
+console.log("get- Attendance Data");
+if()
+response.status(200);
+response.setHeader('Content-Type', 'text/html')
+response.render('results');
+});
+
+router.get('/searchset2', function(request,response){//Projects set
+console.log("get- Projects data");
 response.status(200);
 response.setHeader('Content-Type', 'text/html')
 response.render('results');
--- API_App/controllers/users.js
+++ API_App/controllers/users.js
@@ -6,17 +6,23 @@ var Users = require('../models/Users');
 
 
 
-router.get('/user/new',function(request, response){
-  console.log('Request- /new user');
-  //Users.createUser(request.body.email);
+router.get('/login',function(request, response){
+  console.log('Request- /login');
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('user_details');
 });
 
+router.get('/logout',function(request, response){
+  console.log('Request- /logout');
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('index');
+});
+
 router.post('/results', function(request,response){
 console.log("Post- user w/API Key");
-//Users.crea(request.body.name,request.body.password);
+Users.createUser(request.body.email);
 response.status(200);
 response.setHeader('Content-Type', 'text/html')
 response.render('results');

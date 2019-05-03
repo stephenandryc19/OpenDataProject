97eb619ae8245094b8b6941951857b1364457f23
MaryKate <mary.mcgranahan19@mbpr1315-3280.local>
2019-05-03 11:57:25 -0400

    user post

--- Web_App/controllers/users.js
+++ Web_App/controllers/users.js
@@ -11,6 +11,31 @@ router.get('/user/new',function(request, response){
   response.render('user_details',{user:user});
 });
 
+router.post('/user', function(request,response){
+console.log("Post- user");
+var data=Users.allUsers(function(rows){
+    var new_user=true;
+    for(var i=0; i<rows.length;i++){
+      if(request.query.email==rows[i]["email"]){
+        new_user=false;
+        if(request.query.key==rows[i]["key"]){
+          var user_data={};
+          user_data.email=rows[i].email;
+          response.status(200);
+          response.setHeader('Content-Type', 'text/html')
+          response.render('index');
+          break;
+        }
+        else{
+          response.status(200);
+          response.setHeader('Content-Type', 'text/html')
+          response.render('error');
+          break;
+        }
+      }
+    }
+    }
+});
 /*
 router.post('/user', function(request,response){
 console.log("Post- new user");

95a8e4d2ffff4d69ab455901bc567c90484503b1
Stephen Andryc <Stephen_Andryc@Stephens-MacBook-Air.local>
2019-05-06 09:59:48 -0400

    some stuff from steve part 2

--- API_App/controllers/data.js
+++ API_App/controllers/data.js
@@ -1,21 +1,69 @@
+var fs = require("fs");
+//library to write text to a CSV
+var GoogleSpreadsheet = require('google-spreadsheet');
+//starts Google spreadsheet class
+var creds = require('./../../client_secret.json');
+//accesses client_secret.json file
+var doc = new GoogleSpreadsheet('1nWx5MSPoFFttu9KqVhL0naXFff7KVD5BTjWzfQmjJvc');
+//instantiates from spreadsheet class
 
-var express = require('express');
-var router = express.Router();
+//exports.findNameMatch("P.S./I.S. 338 - BROOKLYN");
 
-var Users = require('../models/Users');
+exports.findNameMatch = function (name,callback) {
+  doc.useServiceAccountAuth(creds, function (err) {
+    doc.getRows(4/*sheet of the spreadsheet*/, function (err, rows) {
+      for (var i = 0; i < rows.length; i ++) {
+        if (rows[i].name==name.trim()) {
+          console.log(rows[i].name);
+          callback(rows[i]);
+        }
+      }
+    });
+  });
+}
 
-router.get('/searchset1', function(request,response){//Attendence set
-console.log("get- Attendance Data");
-response.status(200);
-response.setHeader('Content-Type', 'text/html')
-response.render('results');
+exports.listSchools = function (callback) {
+  doc.useServiceAccountAuth(creds, function (err) {
+    doc.getRows(4/*sheet of the spreadsheet*/, function (err, rows) {
+      for (var i = 0; i < rows.length; i ++) {
+          callback(rows[i]);
+      }
+    });
+  });
+}
+
+exports.findBoroughMatch = function (borough,callback) {
+  doc.useServiceAccountAuth(creds, function (err) {
+    doc.getRows(4/*sheet of the spreadsheet*/, function (err, rows) {
+      for (var i = 0; i < rows.length; i ++) {
+        if (rows[i].borough==borough.trim()) {
+          console.log(rows[i].name+" "+rows[i].borough);
+          callback(rows[i]);
+        }
+      }
+    });
+  });
+}
+
+/*
+router.get('/searchset2', function(request, response){
+  console.log('Request- search set 2');
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('index');
 });
 
-router.get('/searchset2', function(request,response){//Projects set
-console.log("get- Projects data");
+router.post('/savedata', function(request,response){
+console.log("Post- save data");
 response.status(200);
 response.setHeader('Content-Type', 'text/html')
-response.render('results');
+response.render('settings');
 });
 
-module.exports = router;
+router.delete('/mydata/:id', function(request, response){
+  console.log('Request- search set 2');
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('settings');
+});
+*/

bb0dd9a9c5c22904d112c6ae32de7ff5c11f1823
MaryKate <mary.mcgranahan19@mbpr1315-3280.local>
2019-05-06 10:23:21 -0400

    hi

--- API_App/controllers/data.js
+++ API_App/controllers/data.js
@@ -45,25 +45,5 @@ exports.findBoroughMatch = function (borough,callback) {
   });
 }
 
-/*
-router.get('/searchset2', function(request, response){
-  console.log('Request- search set 2');
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render('index');
-});
 
-router.post('/savedata', function(request,response){
-console.log("Post- save data");
-response.status(200);
-response.setHeader('Content-Type', 'text/html')
-response.render('settings');
-});
-
-router.delete('/mydata/:id', function(request, response){
-  console.log('Request- search set 2');
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render('settings');
-});
-*/
+module.exports = router;
